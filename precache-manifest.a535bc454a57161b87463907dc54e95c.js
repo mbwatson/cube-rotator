fb14a5ad0ed53e597ad980794cd680478565b97c
Matt Watson <mattwat@gmail.com>
2020-02-09 18:46:44 -0500

    Updates

---
+++ precache-manifest.a535bc454a57161b87463907dc54e95c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acd483b9a6a238b8b87dc0549abbd512",
+    "url": "/cube-rotator/index.html"
+  },
+  {
+    "revision": "8eb5a8d2bd986f8336a8",
+    "url": "/cube-rotator/static/css/main.42aa9447.chunk.css"
+  },
+  {
+    "revision": "df55b910e99743a8f406",
+    "url": "/cube-rotator/static/js/2.1804631f.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/cube-rotator/static/js/2.1804631f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8eb5a8d2bd986f8336a8",
+    "url": "/cube-rotator/static/js/main.8203e8db.chunk.js"
+  },
+  {
+    "revision": "093260720ed10cf10557",
+    "url": "/cube-rotator/static/js/runtime-main.7ae077df.js"
+  }
+]);
